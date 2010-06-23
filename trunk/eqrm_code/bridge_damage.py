e01a6a8f862a8c8d71f76b4ce60a2d893c9101ea
rwilson <rwilson@9b577d95-b96c-4151-4eaa-79402b10d87c>
2010-06-23 07:01:55 +0000

    Changes to bridge damage state calculations, and test cases.

--- trunk/eqrm_code/bridge_damage.py
+++ trunk/eqrm_code/bridge_damage.py
@@ -48,12 +48,6 @@ DataFilePath = eqrm_filesystem.Resources_Data_Path
 
 
 ######
-# Damage states
-######
-
-States = ['None', 'Slight', 'Moderate', 'Extensive', 'Complete']
-
-######
 # Models recognized by this module.
 ######
 
@@ -62,22 +56,22 @@ ModelLinear = 'LINEAR'
 
 ################################################################################
 
-def bridge_damage(model, CLASS, sa_0_3, sa_1_0, skew, num_spans):
-    """Top-level function to calculate bridge damage state.
+def bridge_states(CLASS, sa_0_3, sa_1_0, skew, num_spans, model=ModelLinear):
+    """Top-level function to calculate bridge damage states.
 
-    model      the model to use when estimating damage (string)
     CLASS      bridge class - a string like 'hwb1'
-    sa_0_3     spectral acceleration at the bridge, T=0.3s
-    sa_1_0     spectral acceleration at the bridge, T=1.0s
+    sa_0_3     1D array of spectral acceleration at the bridge, T=0.3s
+    sa_1_0     1D array of spectral acceleration at the bridge, T=1.0s
     skew       bridge skew
     num_spans  number of spans in the bridge
+    model      the model to use when estimating damage (string)
 
-    Returns an index into the States iterable showing damage state.
+    Returns a tuple (slight, moderate, extensive, complete) of state
+    probabilities.  Each probability is in the range [0.0, 1.0].
     """
 
-    model = str(model)
-
-    model_func = ModelFunctions.get(model, invalid_model_func)
+    model = str(model).upper()
+    model_func = ModelStateFunctions.get(model, invalid_model_func)
 
     return model_func(model, CLASS, sa_0_3, sa_1_0, skew, num_spans)
 
@@ -85,43 +79,18 @@ def bridge_damage(model, CLASS, sa_0_3, sa_1_0, skew, num_spans):
 # Individual damage functions - called by top-level routine above
 ################################################################################
 
-def bridge_damage_ModelLinear(model, CLASS, sa_0_3, sa_1_0, skew, num_spans):
-    """Calculate a bridge damage probability Classification.
+def bridge_states_ModelLinear(model, CLASS, sa_0_3, sa_1_0, skew, num_spans):
+    """Calculate the bridge damage probability states for LINEAR model.
 
     model      the model to use when estimating damage (ignored)
     CLASS      bridge class - string like 'hwb1'
-    sa_0_3     spectral acceleration at the bridge, T=0.3s
-    sa_1_0     spectral acceleration at the bridge, T=1.0s
+    sa_0_3     1D array of spectral acceleration at the bridge, T=0.3s
+    sa_1_0     1D array of spectral acceleration at the bridge, T=1.0s
     skew       bridge skew
     num_spans  number of spans in the bridge
 
-    Returns an index into the States iterable showing damage state.
-    """
-
-    spi = linear_bridge_damage(CLASS, sa_0_3, sa_1_0, skew, num_spans)
-    return get_random_state_from_iterable(spi)
-
-def invalid_model_func(model, *args):
-    """Function called for unrecognized model string."""
-
-    msg = 'Unrecognized bridge damage model: %s' % model
-    raise RuntimeError(msg)
-
-################################################################################
-# Low-level damage functions
-################################################################################
-
-def linear_bridge_damage(CLASS, sa_0_3, sa_1_0, skew, num_spans):
-    """Calculate a bridge damage probability classification linear model.
-
-    CLASS      bridge class - string like 'hwb1'
-    sa_0_3     spectral acceleration at the bridge, T=0.3s
-    sa_1_0     spectral acceleration at the bridge, T=1.0s
-    skew       bridge skew
-    num_spans  number of spans in the bridge
-
-    Returns an iterable of state propabilities:
-        (none, slight, moderate, extensive, complete)
+    Returns an array of state probabilities:
+        (slight, moderate, extensive, complete)
     """
 
     # convert user CLASS to uppercase, just in case
@@ -149,7 +118,7 @@ def linear_bridge_damage(CLASS, sa_0_3, sa_1_0, skew, num_spans):
     # adjust for Ishape value
     Kshape = 2.5 * (sa_1_0/sa_0_3)
     if Ishape == 0:
-        Kshape = 1.0
+        Kshape = num.ones_like(sa_1_0)
 
     # now calculate the median fragility curve values for states 2 through 5
     A2 = Kshape * a2
@@ -158,42 +127,23 @@ def linear_bridge_damage(CLASS, sa_0_3, sa_1_0, skew, num_spans):
     A5 = Kskew * K3d_value * a5
 
     # now get probability of exceedance for each damage state
-    P_slight = scipy.stats.norm.cdf(math.log(sa_1_0/A2)/Beta)
-    P_moderate = scipy.stats.norm.cdf(math.log(sa_1_0/A3)/Beta)
-    P_extensive = scipy.stats.norm.cdf(math.log(sa_1_0/A4)/Beta)
-    P_complete = scipy.stats.norm.cdf(math.log(sa_1_0/A5)/Beta)
+    P_slight = scipy.stats.norm.cdf(num.log(sa_1_0/A2)/Beta)
+    P_moderate = scipy.stats.norm.cdf(num.log(sa_1_0/A3)/Beta)
+    P_extensive = scipy.stats.norm.cdf(num.log(sa_1_0/A4)/Beta)
+    P_complete = scipy.stats.norm.cdf(num.log(sa_1_0/A5)/Beta)
 
-    return (1.0-P_slight, P_slight-P_moderate, P_moderate-P_extensive,
-            P_extensive-P_complete, P_complete-0.0)
+    return num.array((P_slight-P_moderate, P_moderate-P_extensive,
+                      P_extensive-P_complete, P_complete))
+
+def invalid_model_func(model, *args):
+    """Function called for unrecognized model string."""
+
+    msg = 'Unrecognized bridge damage model: %s' % model
+    raise RuntimeError(msg)
 
 ################################################################################
 # Utility routines
 ################################################################################
-
-def get_random_state_from_iterable(spi, v=None):
-    """Randomly choose a state from a state probability iterable.
-    
-    spi  the state probability tuple (p1, p2, ...)
-         this may have any number of probability values
-    v    random number in [0.0, 1.0) PURELY FOR TESTING
-         DO NOT USE THIS IN PRODUCTION CODE
-
-    We assume sum(spi) == 1.0.
-
-    Returns the index of the chosen state.
-    """
-
-    # choose a float in [0.0, 1.0)
-    if v is None:
-        v = random.random()
-
-    accum = 0
-    for (i, p) in enumerate(spi):
-        accum += p
-        if v < accum:
-            return i
-
-    return i
 
 def load_bridge_data():
     """Preload bridge damage data from external files."""
@@ -221,16 +171,10 @@ def load_bridge_data():
                                           columns=['Equation','A','B'],
                                           convert={'A': float, 'B': int})
 
-def get_state_string(index):
-    """Get string describing state from index into States iterable."""
-
-    return States[index]
-
-
 ######
-# Dictionary mapping model to damage function.
+# Dictionaries mapping model to bridge states function.
 ######
 
-ModelFunctions = {ModelLinear: bridge_damage_ModelLinear,
-                 }
+ModelStateFunctions = {ModelLinear: bridge_states_ModelLinear,
+                      }
 
