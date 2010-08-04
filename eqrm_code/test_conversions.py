14baf17d1bf05427d1f227b2fedbc79c1af54a13
rwilson <rwilson@9b577d95-b96c-4151-4eaa-79402b10d87c>
2010-08-04 04:48:49 +0000

    Added calc_depth_to_top() function and test code.

--- eqrm_code/test_conversions.py
+++ eqrm_code/test_conversions.py
@@ -1,7 +1,8 @@
 
 import unittest
+import math
 
-from scipy import array,allclose
+from scipy import array, allclose
 
 from eqrm_code.conversions import conversion_functions
 from eqrm_code.xml_interface import Xml_Interface
@@ -43,8 +44,54 @@ class Test_Conversions(unittest.TestCase):
         length = area/width
         self.assert_ (allclose([length],[20]))
         
+
+    def test_calc_depth_to_top(self):
+        """Test the calc_depth_to_top() function."""
+
+        calc_depth_to_top = conversion_functions['calc_depth_to_top']
+
+        # test an invalid datatype case (raises AssertError)
+        depth = 10.0
+        width = 20.0
+        delta = 0
+        self.failUnlessRaises(AssertionError,
+                              calc_depth_to_top, depth, width, delta)
+
+        # test an invalid shape case (raises AssertError)
+        depth = array([10.0, 20.0])
+        width = array([20.0])
+        delta = array([0])
+        self.failUnlessRaises(AssertionError,
+                              calc_depth_to_top, depth, width, delta)
+
+        # test a single value, delta=0, depth_to_top == depth
+        depth = array([10.0])
+        width = array([20.0])
+        delta = array([0])
+        expected = depth		# Ztor == depth if delta == 0
+        Ztor = calc_depth_to_top(depth, width, delta)
+        self.failUnless(allclose(Ztor, expected))
         
-#-------------------------------------------------------------
+        # test many cases in one array set
+        Ztor_45 = 20.0-10.0/2*math.sin(math.pi*2*45.0/360) # depth at 45 degrees
+        #                  +- horizontal, Ztor==depth
+        #                  |     +- vertical, Ztor=depth-width/2
+        #                  |     |     +- 30 degrees, Ztor=depth-width/4
+        #                  |     |     |     +- vertical, Ztor=depth-width/2
+        #                  |     |     |     |            (negative depth)
+        #                  V     V     V     V
+        depth =    array([20.0, 20.0, 20.0, 10.0, 20.0])
+        width =    array([10.0, 10.0, 10.0, 30.0, 10.0])
+        delta =    array([ 0.0, 90.0, 30.0, 90.0, 45.0])
+        expected = array([20.0, 15.0, 17.5, -5.0, Ztor_45])
+        Ztor = calc_depth_to_top(depth, width, delta)
+        msg = 'Expected %s same as %s' % (str(Ztor), str(expected))
+        self.failUnless(allclose(Ztor, expected), msg)
+        
+
+        
+################################################################################
+
 if __name__ == "__main__":
     suite = unittest.makeSuite(Test_Conversions,'test')
     runner = unittest.TextTestRunner()
