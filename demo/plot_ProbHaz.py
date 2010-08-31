6fe06ab93acde11151c8b9aadaf37147a7f41a45
dgray <dgray@9b577d95-b96c-4151-4eaa-79402b10d87c>
2010-08-31 16:28:39 +0000

    implementing the new style zone xml file.  Removing obsolete atts in the control file.

--- demo/plot_ProbHaz.py
+++ demo/plot_ProbHaz.py
@@ -31,12 +31,8 @@ site_indexes = 2255
 # Scenario input
 
 # Probabilistic input
-prob_azimuth_in_zones = [10, 30, 70, 100, 150, 15]
-prob_delta_azimuth_in_zones = [5, 10, 20, 25, 50, 0]
 prob_min_mag_cutoff = 4.5
-prob_number_of_mag_sample_bins = 15
 prob_number_of_events_in_zones = [5000, 1000, 1000, 3000, 1000, 1000]
-prob_dip_in_zones = [35, 40, 45, 50, 55, 60]
 
 # Attenuation
 atten_models = ['Sadigh_97']
