a54840bbb759da027400410c402f46bb592cffa5
rwilson <rwilson@9b577d95-b96c-4151-4eaa-79402b10d87c>
2010-09-23 03:15:46 +0000

    Added extra functionality to work with the code in fig_xyz_histogram().

--- trunk/eqrm_code/plotting/plot_barchart.py
+++ trunk/eqrm_code/plotting/plot_barchart.py
@@ -13,11 +13,12 @@ import sys
 
 import eqrm_code.plotting.util_timestamp as pts
 import eqrm_code.plotting.util_user_annotation as pua
+import eqrm_code.plotting.utilities as util
 
 
 def plot_barchart(data, output_file=None, title='', xlabel='', ylabel='',
-                  xrange=None, yrange=None, grid=True,
-                  show_graph=False, annotate=[]):
+                  xrange=None, yrange=None, grid=True, show_graph=False,
+                  bin_width=None, bardict=None, annotate=[]):
     """Plot a barchart.  Optionally save a file or show the graph (or both).
 
     Inputs:
@@ -28,10 +29,14 @@ def plot_barchart(data, output_file=None, title='', xlabel='', ylabel='',
     title          the graph title string, if supplied
     xlabel         label to put on the X axis
     ylabel         label to put on the Y axis
-    xrange         a scalar indicating maximum X value to plot
-    yrange         a scalar indicating maximum Y value to plot
+    xrange         a scalar or iterable indicating min[,max] X value to plot
+    yrange         a scalar or iterable indicating min[,max] Y value to plot
     grid           draw a grid on graph if True
     show_graph     show graph on screen if True
+    bin_width      width of the bins
+    bardict        a dictionary containing extra parameters to the
+                   matplotlib.pyplot.bar() routine.  Params are defined at
+     http://matplotlib.sourceforge.net/api/pyplot_api.html#matplotlib.pyplot.bar
     annotate       an iterable like [(x, y, ann, dict), ...] where
                        x    is X screen coordinate
                        y    is Y screen coordinate
@@ -42,8 +47,7 @@ def plot_barchart(data, output_file=None, title='', xlabel='', ylabel='',
                    NOTE: if annotate=None is used, no annotation occurs.
                          if annotate=[] is used, auto annotation is generated:
                              timestamp
-                             clipping
-                             max input data value
+                             X and Y range limits
 
     """
 
@@ -70,16 +74,25 @@ def plot_barchart(data, output_file=None, title='', xlabel='', ylabel='',
         fig = plt.figure()
         ax = fig.add_subplot(111)
        
-        # handle axis ranges
-        if xrange:
-            plt.xticks(scipy.arange(xrange+1))
-
-        if yrange:
-            plt.yticks(scipy.arange(yrange+1))
+        # get bardict into correct form for bar() call below
+        bar_kwargs = {}
+        if bardict:
+            bar_kwargs = bardict
 
         # plot the data
-        plt.bar(data[:,0], data[:,1], align='center')
-        #plt.bar(data[:,0], data[:,1])
+        if bin_width:
+            plt.bar(data[:,0], data[:,1], width=bin_width, align='center',
+                    **bar_kwargs)
+        else:
+            plt.bar(data[:,0], data[:,1], align='center', **bar_kwargs)
+
+        # set X and Y range limits
+        xrange = util.get_canonical_range(xrange)
+        yrange = util.get_canonical_range(yrange)
+        if xrange:
+            plt.xlim(xrange)
+        if yrange:
+            plt.ylim(yrange)
 
         # do X and Y labels
         plt.xlabel(xlabel)
@@ -93,6 +106,16 @@ def plot_barchart(data, output_file=None, title='', xlabel='', ylabel='',
         # add system annotations: time generated, etc
         if isinstance(annotate, list):
             pts.plot_timestamp(fig)
+
+        # add range limits annotations
+        if annotate is None:
+            annotate = []
+        if xrange:
+            annotate.append((0.02, 0.05,
+                             'X range forced to (%.2f,%.2f)' % xrange))
+        if yrange:
+            annotate.append((0.02, 0.03,
+                              'Y range forced to (%.2f,%.2f)' % yrange))
 
         # add user annotations, if any
         pua.plot_user_annotation(fig, annotate)
