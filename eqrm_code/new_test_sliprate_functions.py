9ee6b3b5a1c021ba74ec4f9a56c884559d07de65
vnewey <vnewey@9b577d95-b96c-4151-4eaa-79402b10d87c>
2010-10-01 08:15:18 +0000

    Graphs the resulting recurrence rates.

--- eqrm_code/new_test_sliprate_functions.py
+++ eqrm_code/new_test_sliprate_functions.py
@@ -3,8 +3,9 @@ import sys
 
 import unittest
 
-from scipy import exp, log, array, sum, allclose, r_
-
+from scipy import exp, log, array, sum, allclose, r_, random, cumsum
+import matplotlib.pylab as plt
+from numpy import arange,exp
 
 from eqrm_code.event_set import Event_Set
 from eqrm_code.source_model import Source_Model, Source_Zone_Polygon        
@@ -24,11 +25,12 @@ class Test_Sliprate_functions(unittest.TestCase):
         prob_min_mag_cutoff = 4.0
         slip_rate_mm=2.0
         area_kms= float(30*10)
-        prob_number_of_mag_sample_bins=10
+        prob_number_of_mag_sample_bins=200
         b = 1.
         bin_centroids = make_bins(prob_min_mag_cutoff,max_magnitude,prob_number_of_mag_sample_bins)
-        event_bins=array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-        
+        event_bins=r_[0:prob_number_of_mag_sample_bins]
+        event_bins=sorted(event_bins)
+    
         print "Params: \n \
         max_magnitude = ",max_magnitude, "\n \
         prob_min_mag_cutoff = ",prob_min_mag_cutoff," \n \
@@ -51,15 +53,53 @@ class Test_Sliprate_functions(unittest.TestCase):
         pdfs= calc_activities_from_slip_rate_Characteristic(bin_centroids, b, 
                                                               prob_min_mag_cutoff, 
                                                               max_magnitude)
-        print "\npdfs  from -slip rate Characteristic: ", pdfs
+        #print "\npdfs  from -slip rate Characteristic: ", pdfs
          
         event_activity_source = array(
                 [(A_minCharacteristic*pdfs[z]/(sum(where(
                 event_bins == z, 1,0)))) for z in event_bins])
-        print "\nevent activity: ", event_activity_source
+        #print "\nevent activity: ", event_activity_source
         print "\nsum event activity: ", sum(event_activity_source)
         
-    
+        t = arange(0.01, 20.0, 0.01)
+        plt.figure()
+        plt.semilogy(t, exp(-t/5.0))
+        plt.title('Characteristic PDF')
+        plt.xlabel('magnitude')
+        plt.ylabel('probability')
+        plt.plot(bin_centroids[0:-1],pdfs[0:-1])
+        plt.ylim(0.00,0.04)
+        plt.xlim(4,7)
+        plt.savefig('pdf_characteristic.png')
+
+        plt.figure()
+        plt.semilogy(t, exp(-t/5.0))
+        plt.title('Characteristic Event Activity')
+        plt.xlabel('magnitude')
+        plt.ylabel('events/year')
+        plt.plot(bin_centroids[0:-1],event_activity_source[0:-1])
+        plt.ylim(0.00,0.001)
+        plt.xlim(4,7)
+        plt.savefig('characteristic_event_activity.png')
+        
+        plt.figure()
+        plt.semilogy(t, exp(-t/5.0))
+        event_activity_source=event_activity_source[::-1]
+        bin_centroids=bin_centroids[::-1]
+        cumulative_event_activity= cumsum(event_activity_source)
+        plt.title('Cumulative Characteristic Event Activity')
+        plt.xlabel('magnitude')
+        print " (max = " , max(cumulative_event_activity), ")"
+        plt.ylabel('cumulative events/year')
+        plt.plot(bin_centroids[0:-1],cumulative_event_activity[:-1])
+        plt.ylim(0.00,0.04)
+        plt.xlim(4,7)
+        plt.savefig('Cumulative_chrctrstc_event_activity.png')
+        
+
+       
+        
+
 #-------------------------------------------------------------
 if __name__ == "__main__":
     suite = unittest.makeSuite(Test_Sliprate_functions,'test')
