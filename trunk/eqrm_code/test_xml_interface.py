484eb24db010e6d147e0df26c7383f55bb28e62d
rwilson <rwilson@9b577d95-b96c-4151-4eaa-79402b10d87c>
2010-10-05 03:00:31 +0000

    Added another test.

--- trunk/eqrm_code/test_xml_interface.py
+++ trunk/eqrm_code/test_xml_interface.py
@@ -48,38 +48,84 @@ def build_xml():
 
 
 class Test_Xml_Interface(unittest.TestCase):
+    def setUp(self):
+        # correctly formed old-style XML
+        str1 = '\n'.join(['<Event magnitude_type="Mw">',
+                          '  <polygon>',
+                          '    <boundary>',
+                          '-20 120',
+                          '-30 120',
+                          '-20 120',
+                          '    </boundary>',
+                          '    <depth distribution="constant" mean="10"></depth>',
+                          '  </polygon>',
+                          '  <polygon>',
+                          '    <boundary>',
+                          '-30 121',
+                          '-35 122',
+                          '-30 135',
+                          '-30 121',
+                          '    </boundary>',
+                          '    <exclude>',
+                          '-32 121',
+                          '-34 122',
+                          '-32 121',
+                          '    </exclude>',
+                          '    <exclude>',
+                          '-35 121',
+                          '-36 122',
+                          '-35 121',
+                          '    </exclude>',
+                          '    <depth distribution="constant" mean="5" />',
+                          '  </polygon>',
+                          '</Event>'])
+        self.xml = Xml_Interface(string=str1)
+
+        # similar to above but simplified, wrong name for top-level tag
+        str2 = '\n'.join(['<EventX magnitude_type="Mw">',
+                          '  <polygon>',
+                          '    <depth distribution="constant" mean="10"></depth>',
+                          '  </polygon>',
+                          '  <polygon>',
+                          '    <depth distribution="constant" mean="5" />',
+                          '  </polygon>',
+                          '</EventX>'])
+        self.xml2 = Xml_Interface(string=str2)
+
+    def tearDown(self):
+        self.xml.unlink()
+        self.xml2.unlink()
+
     def test_attributes(self):
-        xml = build_xml()
-        assert xml['Event'][0].attributes['magnitude_type']=='Mw'
-        #print "xml['Event']", xml['Event']
-        xml.unlink()
+        assert self.xml['Event'][0].attributes['magnitude_type']=='Mw'
         
     def test_getting_polygons(self):
-        xml = build_xml()
-        assert  len(xml['polygon'])==2
-        xml.unlink()
-        #log.log("YEAH$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-        #log.log("IF YOU ARE GETTING THIS MESSAGE")
-        #log.log("THE console_logging_level IS TOO LOW.")
-
+        assert  len(self.xml['polygon'])==2
 
     def test_getting_excludes(self):
-        xml = build_xml()
-        assert len(xml['polygon'][0]['exclude'])==0
-        assert len(xml['polygon'][1]['exclude'])==2
-        xml.unlink()
-
+        assert len(self.xml['polygon'][0]['exclude'])==0
+        assert len(self.xml['polygon'][1]['exclude'])==2
 
     def test_getting_array(self):
-        xml = build_xml()
-        exclude_array = xml['polygon'][1]['exclude'][0].array
+        exclude_array = self.xml['polygon'][1]['exclude'][0].array
         expected_exclude_array=[[-32.0,121],[-34,122],[-32,121]]
         assert allclose(asarray(exclude_array),asarray(expected_exclude_array))
-        xml.unlink()
 
     def Xtest_build_zone_xml(self):
         # Looking into something different
         root = build_xml()
+
+    def test_top_level_tag_name(self):
+        """Test getting some info about the XML document.
+
+        Mainly to test various operations we will use.
+        """
+
+        # test getting name of top-level tag
+        # NOTE: There *must* be a better way to do this!?
+        top_tag = self.xml.xml_node.documentElement.nodeName
+        msg = "Expected top-level tag of 'Event', got '%s'" % top_tag
+        self.failUnlessEqual(top_tag, 'Event', msg)
 
 #-------------------------------------------------------------
 if __name__ == "__main__":
