5a11de27ed6bb4fde37109da091921e3ff56fe68
dgray <dgray@9b577d95-b96c-4151-4eaa-79402b10d87c>
2011-03-11 06:22:50 +0000

    Autogenerate figs for the manual

--- eqrm_code/create_demo_plot_data.py
+++ eqrm_code/create_demo_plot_data.py
@@ -1,8 +1,11 @@
 
 import os
+import string
 
 from eqrm_code import eqrm_filesystem
 from eqrm_code import analysis
+from eqrm_code import parse_in_parameters
+
 
 plot_demo_run_names = ['plot_ProbRisk.py', 'plot_ProbHaz.py', 'plot_ScenGM.py',
                  'plot_ScenRisk.py']
@@ -13,7 +16,8 @@ def demo_run(verbose=True, plot_dir=eqrm_filesystem.demo_plot_path,
         print 'STARTING'  
 
     plot_dir = os.path.join(plot_dir) # Why do this?
-    current_dir = os.getcwd()
+    current_dir = os.path.abspath(os.getcwd())
+    print "current", current
     os.chdir(plot_dir)
     if verbose:
         print 'Parameter file names = ',run_names
@@ -32,6 +36,34 @@ def demo_run(verbose=True, plot_dir=eqrm_filesystem.demo_plot_path,
         
         if verbose:
             print 'FINISH'
+    os.chdir(current_dir)
+
+    
+def run_scenarios(dir, file_start='plot_', extension='.py'):
+    """
+    Run all of the EQRM control files in a directory.
+    """
+    
+    # Make the current dir the dir this file is in.
+    #known_dir, tail = os.path.split(__file__)
+    #if known_dir == '':
+      #  known_dir = '.'
+    known_dir = os.path.abspath(dir)
+    current_dir = os.getcwd()
+    os.chdir(known_dir)
+    plot_files = os.listdir(known_dir)
+    plot_files = [x for x in plot_files if x.endswith(extension)]
+    plot_files = [x for x in plot_files if 0 == string.find(x,file_start)]
+    for plot_file in plot_files:
+        THE_PARAM_T = parse_in_parameters.create_parameter_data(plot_file)
+        plot_output_dir = os.path.join(THE_PARAM_T['output_dir'])
+        files = os.listdir(plot_output_dir)
+        demo_files = [x for x in files if x[-3:] == 'txt']
+        #print "len(demo_files)", len(demo_files)
+        if len(demo_files) <= 4: # 4 is the magic number
+            # Assume the plot data has not been created.
+            # Run the plot scenario
+            analysis.main(os.path.join(plot_file))
     os.chdir(current_dir)
 
 if __name__ == '__main__':
