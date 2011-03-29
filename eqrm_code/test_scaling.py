18bc3dc14fa6346f78eda822cf2377145f090b0e
dgray <dgray@9b577d95-b96c-4151-4eaa-79402b10d87c>
2011-03-29 04:58:48 +0000

    Adding Leonard_SCR scaling rules.

--- eqrm_code/test_scaling.py
+++ eqrm_code/test_scaling.py
@@ -2,10 +2,11 @@
 import unittest
 import math
 
-from scipy import array, allclose, exp, log, power, asarray
+from scipy import array, allclose, exp, power, asarray
 
 from eqrm_code.conversions import conversion_functions
 from eqrm_code.scaling import *
+from eqrm_code.scaling_functions import *
 
 class Test_scaling(unittest.TestCase):
     def test_modified_Wells_and_Coppersmith_94_rup_width(self):
@@ -120,6 +121,76 @@ class Test_scaling(unittest.TestCase):
         correct = [ 0.1, ]
         assert allclose(correct, width)
         
+    def test_Leonard_SCR_area(self):
+        
+        Mw = array([5.5])
+        scaling_dic = {'scaling_rule':'Leonard_SCR'}           
+        area = scaling_calc_rup_area(Mw, scaling_dic)  
+        correct = [ 20.73322, ] # From Scaling_Relations_Leonard_2010.xls
+        assert allclose(correct, area)
+        correct = Leonard_Mw_to_Area(Mw)
+        assert allclose(correct, area)
+        
+        Mw = array([5.5, 6.5, 7.0, 7.5])
+        scaling_dic = {'scaling_rule':'Leonard_SCR'}           
+        area = scaling_calc_rup_area(Mw, scaling_dic)  
+        correct = Leonard_Mw_to_Area(Mw)
+        assert allclose(correct, area)
+
+    def test_Leonard_SCR_width(self):
+        scaling_dic = {'scaling_rule':'Leonard_SCR'} 
+        area = 1.0   
+        Mw = array([5.5])     
+        width = scaling_calc_rup_width(Mw, scaling_dic, dip=None, 
+        rup_area=area) 
+        length = 5.12861 # From Scaling_Relations_Leonard_2010.xls
+        width_actual = area/length
+        assert allclose(width_actual, width)
+            
+        scaling_dic = {'scaling_rule':'Leonard_SCR'} 
+        area = 1.0   
+        Mw = array([8.5])     
+        width = scaling_calc_rup_width(Mw, scaling_dic, dip=None, 
+        rup_area=area) 
+        length = 323593.66/1000 # From Scaling_Relations_Leonard_2010.xls
+        width_actual = area/length
+        assert allclose(width_actual, width)
+            
+        scaling_dic = {'scaling_rule':'Leonard_SCR'} 
+        area = 5100.0   
+        Mw = array([5.5])   
+        max_rup_width = 25.0  
+        width = scaling_calc_rup_width(Mw, scaling_dic, dip=None, 
+        rup_area=area, max_rup_width=max_rup_width) 
+        length = 5.12861 # From Scaling_Relations_Leonard_2010.xls
+        width_actual = area/length
+        assert allclose(width, max_rup_width)
+        
+    def test_Leonard_SCR_constants(self):
+    
+        Mw = array([4., 8.]) 
+        e, f = Leonard_SCR_constants(Mw)
+        assert allclose(e, [1.5/3., 1.5/2.5])
+        assert allclose(f, [(1.5*6.07-6.39)/3.-3.0, (1.5*6.07-8.08)/2.5-3.0])
+        
+        
+def Mw_to_Mo(Mw):
+    """
+    Returns:
+      Mo units Nm
+    """   
+    c = 1.5
+    d = 9.105
+    Mo = 10**(c*Mw + d)
+    return Mo
+    
+def Leonard_Mw_to_Area(Mw):
+    a = 1.5
+    b = 6.38
+    Mo = Mw_to_Mo(Mw)
+    Area = ((Mo/10**b)**(1/a))/1000000
+    return Area
+    
 ################################################################################
 
 if __name__ == "__main__":
