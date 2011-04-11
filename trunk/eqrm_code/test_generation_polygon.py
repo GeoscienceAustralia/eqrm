bfe4f7846e9e093915503e77997d694f346438b3
dgray <dgray@9b577d95-b96c-4151-4eaa-79402b10d87c>
2011-04-11 06:10:38 +0000

    Removing Ml as a magnitude type option in the source xml files.

--- trunk/eqrm_code/test_generation_polygon.py
+++ trunk/eqrm_code/test_generation_polygon.py
@@ -85,10 +85,9 @@ class Test_Generation_polygon(unittest.TestCase):
                     (152.1700, -32.7500),
                     (151.4300, -33.4500),
                     (151.1500, -32.4000)]
-        exclude = None # This is not tested
-                       #[(151.1500, -32.4000),
-                       #(152.1700, -32.7500),
-                       #(151.4300, -33.4500)]
+        exclude =  None #[(151.1500, -32.4000),
+                    #(152.1700, -32.7500),
+                    #(151.4300, -33.4500)]
         depth_top_seismogenic_dist = {'distribution':'constant',
                             'mean':7}
         depth_bottom_seismogenic_dist = {'distribution':'constant',
@@ -144,6 +143,131 @@ class Test_Generation_polygon(unittest.TestCase):
             
         os.remove(file_name)
 
+        
+    def test_polygons_from_xml_horspool_FileError_on_Ml(self):
+        
+        handle, file_name = tempfile.mkstemp('.xml', __name__+'_')
+        os.close(handle)
+        handle = open(file_name,'w')
+        
+        sample = """<source_model_zone magnitude_type="Ml">
+  <zone 
+  area = "5054.035" 
+  name = "bad zone"
+  event_type = "crustal fault">
+    
+    <geometry 
+       azimuth= "45" 
+       delta_azimuth= "5" 
+       dip= "35"
+       delta_dip = "5"
+       depth_top_seismogenic = "7"
+       depth_bottom_seismogenic = "15.60364655">
+      <boundary>
+	  151.1500 -32.4000  
+	  152.1700 -32.7500
+	  151.4300 -33.4500  
+	  151.1500 -32.4000
+      </boundary>
+      <excludes>
+	  151.1500 -32.4000    
+	  152.1700 -32.7500   
+	  151.4300 -33.4500 
+      </excludes>
+    </geometry>
+    
+    <recurrence_model
+      distribution = "bounded_gutenberg_richter"
+      recurrence_min_mag = "3.4" 
+      recurrence_max_mag = "5.4" 
+      A_min= "0.568" 
+      b = "1">
+      <event_generation 
+      generation_min_mag = "3.3"
+	  number_of_mag_sample_bins = "15" 
+	  number_of_events = "1000" />
+    </recurrence_model>
+    
+    <ground_motion_models 
+       fault_type = "normal" 
+       ground_motion_selection = "crustal fault" />   
+  </zone>
+</source_model_zone>
+"""
+        handle.write(sample)
+        handle.close()
+
+
+        try:
+            generation_polygons, magnitude_type = polygons_from_xml(file_name)
+        except FileError:   
+            os.remove(file_name)
+        else:  
+            os.remove(file_name)
+            self.failUnless(False, "ParameterSyntaxError not raised")
+            
+
+    def test_polygons_from_xml_no_magnitude_type_ok(self):
+        
+        handle, file_name = tempfile.mkstemp('.xml', __name__+'_')
+        os.close(handle)
+        handle = open(file_name,'w')
+        
+        sample = """<source_model_zone>
+  <zone 
+  area = "5054.035" 
+  name = "bad zone"
+  event_type = "crustal fault">
+    
+    <geometry 
+       azimuth= "45" 
+       delta_azimuth= "5" 
+       dip= "35"
+       delta_dip = "5"
+       depth_top_seismogenic = "7"
+       depth_bottom_seismogenic = "15.60364655">
+      <boundary>
+	  151.1500 -32.4000  
+	  152.1700 -32.7500
+	  151.4300 -33.4500  
+	  151.1500 -32.4000
+      </boundary>
+      <excludes>
+	  151.1500 -32.4000    
+	  152.1700 -32.7500   
+	  151.4300 -33.4500 
+      </excludes>
+    </geometry>
+    
+    <recurrence_model
+      distribution = "bounded_gutenberg_richter"
+      recurrence_min_mag = "3.4" 
+      recurrence_max_mag = "5.4" 
+      A_min= "0.568" 
+      b = "1">
+      <event_generation 
+      generation_min_mag = "3.3"
+	  number_of_mag_sample_bins = "15" 
+	  number_of_events = "1000" />
+    </recurrence_model>
+    
+    <ground_motion_models 
+       fault_type = "normal" 
+       ground_motion_selection = "crustal fault" />   
+  </zone>
+</source_model_zone>
+"""
+        handle.write(sample)
+        handle.close()
+
+        generation_polygons, magnitude_type = polygons_from_xml(file_name)
+        
+        self.failUnless(magnitude_type=="Mw",
+            'Failed!')
+            
+        os.remove(file_name)
+                    
+        
     def test_xml_fault_generators(self):
         def dump_fault(fault):
             """Helper function to dump info from FSG object."""
@@ -301,6 +425,193 @@ class Test_Generation_polygon(unittest.TestCase):
         
         self.failUnlessEqual(len(errorIndexes),0)
         
+        
+    def test_xml_fault_generators_magnitude_type(self):
+        def dump_fault(fault):
+            """Helper function to dump info from FSG object."""
+
+            for attr in dir(fault):
+                if attr[0] != '_' and attr != 'name_type_map':
+                    val = eval('fault.%s' % attr)
+                    if isinstance(val, dict):
+                        print('    %s=%s' % (attr, str(val)))
+                    elif isinstance(val, types.MethodType):
+                        pass
+                    else:
+                        print('    %s=%s (%s)' % (attr, str(val), type(val)))
+
+        (handle, file_name) = tempfile.mkstemp('.xml', __name__+'_')
+        os.close(handle)
+        handle = open(file_name,'w')
+        
+        sample = '\n'.join(['<?xml version="1.0" encoding="UTF-8"?>',
+                            '<source_model_fault magnitude_type="Ml">',
+                            '  <fault name="big fault" event_type="crustal fault">',
+                            '    <geometry dip="30" out_of_dip_theta="0"',
+                            '              delta_theta="0"',
+                            '              depth_top_seismogenic="0"',
+                            '              depth_bottom_seismogenic="15"',
+                            '              slab_width="0">',
+                            '      <trace>',
+                            '        <start lat="-17.5" lon="110.0" />',
+                            '        <end lat="-17.0" lon="110.0" />',
+                            '      </trace>',
+                            '    </geometry>',
+                            '    <recurrence_model distribution="bounded_gutenberg_richter"',
+                            '                      recurrence_min_mag="4.0"',
+                            '                      recurrence_max_mag="7.0"',
+                            '                      slip_rate="2.0" b="1">',
+                            '      <event_generation generation_min_mag="4.0"',
+                            '                        number_of_mag_sample_bins="15"',
+                            '                        number_of_events="1500" />',
+                            '    </recurrence_model>',
+                            '  </fault>',
+                            '  <fault name="small fault" event_type="crustal fault">',
+                            '    <geometry dip="90" out_of_dip_theta="10"',
+                            '              delta_theta="5"',
+                            '              depth_top_seismogenic="10"',
+                            '              depth_bottom_seismogenic="50"',
+                            '              slab_width="10">',
+                            '      <trace>',
+                            '        <start lat="-17.0" lon="120.0" />',
+                            '        <end lat="-17.0" lon="120.5" />',
+                            '      </trace>',
+                            '    </geometry>',
+                            '    <recurrence_model distribution="characteristic"',
+                            '                      recurrence_min_mag="4.5"',
+                            '                      recurrence_max_mag="7.5"',
+                            '                      slip_rate="3.0" b="2">',
+                            '      <event_generation generation_min_mag="4.0"',
+                            '                        number_of_mag_sample_bins="15"',
+                            '                        number_of_events="1500" />',
+                            '    </recurrence_model>',
+                            '  </fault>',
+                            '  <fault name="sumba intraplate" event_type="intraplate">',
+                            '    <geometry dip="20" out_of_dip_theta="90"',
+                            '              delta_theta="20"',
+                            '              depth_top_seismogenic="10"',
+                            '              depth_bottom_seismogenic="100"',
+                            '              slab_width="20">',
+                            '      <trace>',
+                            '        <start lat="-0.5" lon="115.0" />',
+                            '        <end lat="0.5" lon=" 116.0" />',
+                            '      </trace>',
+                            '    </geometry>',
+                            '    <recurrence_model distribution="bounded_gutenberg_richter"',
+                            '                      recurrence_min_mag="4.2"',
+                            '                      recurrence_max_mag="7.2"',
+                            '                      A_min="0.58" b="1">',
+                            '      <event_generation generation_min_mag="4.0"',
+                            '                        number_of_mag_sample_bins="15"',
+                            '                        number_of_events="3000" />',
+                            '    </recurrence_model>',
+                            '  </fault>',
+                            '</source_model_fault>'])
+
+        handle.write(sample)
+        handle.close()
+
+        try:
+            (faults, magtype) = xml_fault_generators(file_name)
+        except FileError:   
+            os.remove(file_name)
+        else:  
+            os.remove(file_name)
+            self.failUnless(False, "ParameterSyntaxError not raised")
+            
+        
+    def test_xml_fault_generators_magnitude_type_left_out(self):
+        def dump_fault(fault):
+            """Helper function to dump info from FSG object."""
+
+            for attr in dir(fault):
+                if attr[0] != '_' and attr != 'name_type_map':
+                    val = eval('fault.%s' % attr)
+                    if isinstance(val, dict):
+                        print('    %s=%s' % (attr, str(val)))
+                    elif isinstance(val, types.MethodType):
+                        pass
+                    else:
+                        print('    %s=%s (%s)' % (attr, str(val), type(val)))
+
+        (handle, file_name) = tempfile.mkstemp('.xml', __name__+'_')
+        os.close(handle)
+        handle = open(file_name,'w')
+        
+        sample = '\n'.join(['<?xml version="1.0" encoding="UTF-8"?>',
+                            '<source_model_fault>',
+                            '  <fault name="big fault" event_type="crustal fault">',
+                            '    <geometry dip="30" out_of_dip_theta="0"',
+                            '              delta_theta="0"',
+                            '              depth_top_seismogenic="0"',
+                            '              depth_bottom_seismogenic="15"',
+                            '              slab_width="0">',
+                            '      <trace>',
+                            '        <start lat="-17.5" lon="110.0" />',
+                            '        <end lat="-17.0" lon="110.0" />',
+                            '      </trace>',
+                            '    </geometry>',
+                            '    <recurrence_model distribution="bounded_gutenberg_richter"',
+                            '                      recurrence_min_mag="4.0"',
+                            '                      recurrence_max_mag="7.0"',
+                            '                      slip_rate="2.0" b="1">',
+                            '      <event_generation generation_min_mag="4.0"',
+                            '                        number_of_mag_sample_bins="15"',
+                            '                        number_of_events="1500" />',
+                            '    </recurrence_model>',
+                            '  </fault>',
+                            '  <fault name="small fault" event_type="crustal fault">',
+                            '    <geometry dip="90" out_of_dip_theta="10"',
+                            '              delta_theta="5"',
+                            '              depth_top_seismogenic="10"',
+                            '              depth_bottom_seismogenic="50"',
+                            '              slab_width="10">',
+                            '      <trace>',
+                            '        <start lat="-17.0" lon="120.0" />',
+                            '        <end lat="-17.0" lon="120.5" />',
+                            '      </trace>',
+                            '    </geometry>',
+                            '    <recurrence_model distribution="characteristic"',
+                            '                      recurrence_min_mag="4.5"',
+                            '                      recurrence_max_mag="7.5"',
+                            '                      slip_rate="3.0" b="2">',
+                            '      <event_generation generation_min_mag="4.0"',
+                            '                        number_of_mag_sample_bins="15"',
+                            '                        number_of_events="1500" />',
+                            '    </recurrence_model>',
+                            '  </fault>',
+                            '  <fault name="sumba intraplate" event_type="intraplate">',
+                            '    <geometry dip="20" out_of_dip_theta="90"',
+                            '              delta_theta="20"',
+                            '              depth_top_seismogenic="10"',
+                            '              depth_bottom_seismogenic="100"',
+                            '              slab_width="20">',
+                            '      <trace>',
+                            '        <start lat="-0.5" lon="115.0" />',
+                            '        <end lat="0.5" lon=" 116.0" />',
+                            '      </trace>',
+                            '    </geometry>',
+                            '    <recurrence_model distribution="bounded_gutenberg_richter"',
+                            '                      recurrence_min_mag="4.2"',
+                            '                      recurrence_max_mag="7.2"',
+                            '                      A_min="0.58" b="1">',
+                            '      <event_generation generation_min_mag="4.0"',
+                            '                        number_of_mag_sample_bins="15"',
+                            '                        number_of_events="3000" />',
+                            '    </recurrence_model>',
+                            '  </fault>',
+                            '</source_model_fault>'])
+
+        handle.write(sample)
+        handle.close()
+        
+        (faults, magtype) = xml_fault_generators(file_name)
+        os.remove(file_name)
+            
+        msg = "Expected magnitude type 'Mw', got '%s'" % magtype
+        self.failUnlessEqual(magtype, 'Mw')
+        
+
 ################################################################################
 
 if __name__ == "__main__":
