f478b001b946103d06b196986f7477bf70152b1b
dgray <dgray@9b577d95-b96c-4151-4eaa-79402b10d87c>
2011-04-12 01:54:44 +0000

    Adding scenario to test site_db_tag

--- implementation_tests/standard/TS_risk62/THE_PARAM_T.py
+++ implementation_tests/standard/TS_risk62/THE_PARAM_T.py
@@ -21,12 +21,12 @@ run_type = "risk"
 is_scenario = False
 max_width = 15
 site_tag = "newc" 
-site_db_tag = "" 
+site_db_tag = "_copy" 
 return_periods = [10, 50, 100, 200, 250, 474.56, 500, 974.78999999999996, 1000, 2474.9000000000001, 2500, 5000, 7500, 10000]
 input_dir = r".\implementation_tests\input/" 
 output_dir = r".\implementation_tests\current\TS_risk62/" 
 use_site_indexes = True
-site_indexes = [6067, 3129]
+site_indexes = [1,2]
 zone_source_tag = "TS_risk62" 
 event_control_tag = "use" 
 
