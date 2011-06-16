6ba6f24e92b8508d826ea2e8b2cf1721cb2da2c6
vnewey <vnewey@9b577d95-b96c-4151-4eaa-79402b10d87c>
2011-06-16 06:48:32 +0000

    Removed the old load_event_subset method load_event_set is now used

--- trunk/eqrm_code/output_manager.py
+++ trunk/eqrm_code/output_manager.py
@@ -933,35 +933,6 @@ def load_event_set(saved_dir, site_tag):
     return attribute_dic
      
 
-#def obsolete_load_event_set_subset(saved_dir, site_tag):
-def load_event_set_subset(saved_dir, site_tag):
-    """
-    Load the Mw, and event activity.
-
-    Returns:
-      out: A dictionary with labels 'Mw' and 'event_activity'
-           and values of 1D arrays of this info.
-    """
-    f=open(os.path.join(saved_dir, site_tag + '_event_set.txt'),'r')
-    text = f.read().splitlines()
-    # ditch the comment lines
-    #for i in range(18):
-    #    text.pop(0)
-    
-    #Now only skips 1 line
-    text.pop(0)
-    out = {}
-    for line in text:
-        split_line = line.split(',')
-        out.setdefault('Mw',[]).append(float(split_line[7]))
-        # Trying to get 7.6 instead of 7.5999999999999996
-        # this did not work
-        #out.setdefault('Mw',[]).append(array(split_line[9], dtype=float))
-        out.setdefault('event_activity',[]).append(float(split_line[6]))
-    # Convert to scipy arrays
-    for k, v in out.items():
-        v = array(v,dtype=float)
-    return out      
 
 def save_damage(save_dir, site_tag, damage_name, damage, building_ids,
                  compress=False, parallel_tag=None, write_title=True):
