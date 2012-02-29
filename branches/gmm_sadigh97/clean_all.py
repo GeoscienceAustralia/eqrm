d6584d1d3e6cf7423b662125d4d4ff334b09a302
ben@girorosso.com <ben@girorosso.com@9b577d95-b96c-4151-4eaa-79402b10d87c>
2012-02-29 05:47:42 +0000

    Delete branch gmm_sadigh97, reintegrated with trunk in r977

--- branches/gmm_sadigh97/clean_all.py
+++
@@ -1,6 +0,0 @@
-
-
-#-------------------------------------------------------------
-if __name__ == "__main__":
-    from eqrm_code.clean_all import clean_all_main
-    clean_all_main(verbose=True, prompt=True)
