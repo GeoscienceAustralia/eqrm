17813be6357938333e9cf513b48cc23ccf613647
ben@girorosso.com <ben@girorosso.com@9b577d95-b96c-4151-4eaa-79402b10d87c>
2012-03-15 04:50:44 +0000

    Add option to check_scenarios.py --file_array. Default behaviour is to set file_store.SAVE_METHOD = None as storing the data arrays on small simulations is a slow operation. If --file_array is used then file_store.SAVE_METHOD is untouched and file based data arrays are used.

--- check_scenarios.py
+++ check_scenarios.py
@@ -35,7 +35,19 @@ Title: check_scenarios - Run the implementation scenarios then check
 
 #-------------------------------------------------------------
 if __name__ == "__main__":
+    from optparse import OptionParser
     from eqrm_code.check_scenarios import check_scenarios_main
+    from eqrm_code import file_store
+    
+    parser = OptionParser()
+    parser.add_option('-f', '--file_array', 
+                      dest='file_array',
+                      action='store_true',
+                      help='Turn on file based arrays. Note that the default is on for EQRM.')
+    options, args = parser.parse_args()
+    if not options.file_array:
+        file_store.SAVE_METHOD = None
+    
     check_scenarios_main()
     
     
