fef648fc3568abb67feefcdcae8c68acbd0f10c0
ben@girorosso.com <ben@girorosso.com@9b577d95-b96c-4151-4eaa-79402b10d87c>
2012-03-21 23:45:04 +0000

    Long scenarios now defaults to SAVE_METHOD=None with the option to turn it back on.

--- trunk/long_check_scenario.py
+++ trunk/long_check_scenario.py
@@ -6,11 +6,20 @@ if __name__ == "__main__":
     #import os    
     #os.chdir('..')
     import sys
-    
+    from optparse import OptionParser
     from eqrm_code.check_scenarios import check_scenarios_main, \
                       LONG_SCENARIO_DIR, LONG_STANDARD_DIR, LONG_CURRENT_DIR, \
                       LONG_STANDARD_STRING, LONG_CURRENT_STRING
+    from eqrm_code import file_store
     
+    parser = OptionParser()
+    parser.add_option('-f', '--file_array', 
+                      dest='file_array',
+                      action='store_true',
+                      help='Turn on file based arrays. Note that the default is on for EQRM.')
+    options, args = parser.parse_args()
+    if not options.file_array:
+        file_store.SAVE_METHOD = None
  
     c_failed_missing_file = check_scenarios_main(
         scenario_dir=LONG_SCENARIO_DIR,
