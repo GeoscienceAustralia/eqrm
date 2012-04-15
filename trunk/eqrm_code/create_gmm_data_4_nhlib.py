dbe8943987d87c0d99b373a4d8dfda190a164064
duncan.gray@gmail.com <duncan.gray@gmail.com@9b577d95-b96c-4151-4eaa-79402b10d87c>
2012-04-15 11:33:10 +0000

    expanding functionality

--- trunk/eqrm_code/create_gmm_data_4_nhlib.py
+++ trunk/eqrm_code/create_gmm_data_4_nhlib.py
@@ -4,12 +4,12 @@ Create the ground motion model data for the gem nhlib library.
 This data is used to test the ground motion models.
 """
 
+import math
 import csv
 from scipy import array, reshape
 
 from eqrm_code.ground_motion_specification import Ground_motion_specification
 
-# let's bang it out and then generalise, if need be.
 
 def write_gmm_data_file(model_name, mag, dist, result_type, 
                         periods, file_out,
@@ -25,16 +25,19 @@ def write_gmm_data_file(model_name, mag, dist, result_type,
             the distance vectors, as a list.
       result_type: MEAN or TOTAL_STDDEV
       periods: A list of periods requiring SA values.
+               The first value has to be 0.0.
 
        Mag, distance and periods will be iterated over to give a single SA for
        each combination.  
        file_out: The file name and location of the produced data file.
     """
+    assert periods[0] == 0.0 
     handle = open(file_out, 'wb')
     writer = csv.writer(handle, delimiter=',', quoting=csv.QUOTE_NONE)
     
     # write title
-    title = [mag[0], dist[0], 'result_type', 'component_type'] + periods
+    title = [mag[0], dist[0], 'result_type', 'component_type'] + periods[1:] + \
+        ['pga']
     writer.writerow(title)
 
     # prepare the coefficients
@@ -52,10 +55,34 @@ def write_gmm_data_file(model_name, mag, dist, result_type,
                 mag=array([[[magi]]]),
             distance=array([[[disti]]]),
             coefficient=coeff,
-            sigma_coefficient=sigma_coeff)
-            
+            sigma_coefficient=sigma_coeff) 
+            sa_mod = list(log_mean.reshape(-1)) 
+            sa_mod = [ math.exp(x) for x in sa_mod]
+            sigma_mod = list(log_sigma.reshape(-1)) 
+            if result_type == 'MEAN':
+                row = [magi, disti, result_type, component_type] + \
+                    sa_mod[1:] + \
+                [ sa_mod[0]]
+            else:
+                row = [magi, disti, result_type, component_type] + \
+                    sigma_mod[1:] + \
+                [sigma_mod[0]]
 
+            writer.writerow(row)
+    handle.close()  
 
-    handle.close()
+################################################################################
+
+if __name__ == "__main__":
+    import eqrm_code.create_gmm_data_4_nhlib as cgd
+    file_name = "SOMMERVILLE_YILGARN2009_MEAN.csv"
     
-   
+    gmm = 'Somerville09_Yilgarn'
+    mag = ['rup_mag', [4.0, 5.0, 6.0, 7.0, 8.0]]
+    dist = ['dist_rjb', [0.0002, 5.0, 20.0, 50.0, 100.0, 400.0]]
+    result_type = 'MEAN'
+    periods = [
+    0.0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.075, 0.1, 0.15, 0.2, 0.25, 0.3003,
+   0.4, 0.5, 0.75, 1., 1.4993, 2., 3.003, 4., 5., 7.5019, 10.,]
+    cgd.write_gmm_data_file(gmm, mag, dist, result_type,
+                        periods, file_name)
