6b469c98ce0503477f8982efb717460cd1bae1a1
duncan.gray.ga@gmail.com <duncan.gray.ga@gmail.com@9b577d95-b96c-4151-4eaa-79402b10d87c>
2013-03-18 06:22:24 +0000

    working on estimating memory use

--- trunk/mem_time_tests/reduce_mem/mr_metalog.py
+++ trunk/mem_time_tests/reduce_mem/mr_metalog.py
@@ -4,18 +4,16 @@ Read the multi run logs and predict the memory used.
 
 import os
 
-from eqrm_code.ANUGA_utilities.log_analyser import build_log_info #analyse_log, merge_dicts, LOGFILE
+from eqrm_code.ANUGA_utilities.log_analyser import build_log_info
 from eqrm_code.parse_in_parameters import eqrm_data_home
-
 from eqrm_code.estimator import log_pairs_estimate_mem
+
 
 def create_meta_log(path=None):
     if path == None:
         path = os.path.join(eqrm_data_home(), 'test_national', 
                             'reduce_mem') 
     log_pairs = build_log_info(path) 
-    print "len(log_pairs)", len(log_pairs)
-
     log_pairs_estimate_mem(log_pairs)
 
 #-------------------------------------------------------------
