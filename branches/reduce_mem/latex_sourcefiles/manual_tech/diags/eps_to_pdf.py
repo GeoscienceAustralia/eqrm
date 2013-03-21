37ac6ffdd8a8451875d399c7f3e751fbe38d89e0
duncan.gray.ga@gmail.com <duncan.gray.ga@gmail.com@9b577d95-b96c-4151-4eaa-79402b10d87c>
2013-03-21 04:01:37 +0000

    Removing a branch that has been merged with head

--- branches/reduce_mem/latex_sourcefiles/manual_tech/diags/eps_to_pdf.py
+++
@@ -1,9 +0,0 @@
-import os
-
-files = os.listdir('.')   
-files = [x for x in files if x[-3:] == 'eps']
-
-for file in files:
-    command = 'ps2pdf14 -dPDFSETTINGS=/prepress -dEPSCrop ' + file
-    print command 
-    os.system(command)
--- branches/reduce_mem/latex_sourcefiles/manual_tech/go.sh
+++
@@ -1,5 +0,0 @@
-# Need to do  ./go.sh to run it.
-pdflatex eqrmtech.tex
-bibtex eqrmtech.aux
-pdflatex eqrmtech.tex
-pdflatex eqrmtech.tex
