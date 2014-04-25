0dd846bbe679bbb5cb28abacb77db607745f65e8
duncan.gray@gmail.com <duncan.gray@gmail.com@9b577d95-b96c-4151-4eaa-79402b10d87c>
2014-04-25 06:58:31 +0000

    autopep8ing

--- eqrm_code/test_RSA2MMI.py
+++ eqrm_code/test_RSA2MMI.py
@@ -9,41 +9,42 @@ from scipy import array, allclose
 
 from eqrm_code.RSA2MMI import *
 
+
 class Test_RSA2MMI(unittest.TestCase):
-    
+
     def setUp(self):
         pass
-        
+
     def tearDown(self):
         pass
 
     def test_RSA2MMI(self):
         # This test is bad, since I'm writing it based on the code.
-        data = []   
-        data.append(1.0/980.)
+        data = []
+        data.append(1.0 / 980.)
         # gives  C1 + C2*(log10(1.0))
         # = C1 + C2*0.
-        # = C1 
-        
-        data.append(100.0/980.)
+        # = C1
+
+        data.append(100.0 / 980.)
         # gives  C3 + C4*(log10(10.0))
         # = C3 + C4*2.
-        
+
         data.append(10000000000000.0)
         # hits the max limit
-        
+
         # the min limit can not be hit
-        
+
         period = 1.0
         mmi = rsa2mmi_array(data, period)
-        expected = array([3.23, 0.57 + 2*2.95, 10])
-        #print "expected", expected
-        #print "mmi", mmi
+        expected = array([3.23, 0.57 + 2 * 2.95, 10])
+        # print "expected", expected
+        # print "mmi", mmi
         self.failUnless(allclose(expected, mmi))
-        
-        
+
+
 #-------------------------------------------------------------
 if __name__ == "__main__":
-    suite = unittest.makeSuite(Test_RSA2MMI,'test')
+    suite = unittest.makeSuite(Test_RSA2MMI, 'test')
     runner = unittest.TextTestRunner()
     runner.run(suite)
--- eqrm_code/test_distributions.py
+++ eqrm_code/test_distributions.py
@@ -3,94 +3,94 @@ import sys
 import unittest
 
 from eqrm_code.distributions import distribution_functions
-   
+
 from eqrm_code.util import reset_seed
 
+
 class Test_Distributions(unittest.TestCase):
-    
+
     def setUp(self):
         reset_seed(True)
-        
+
     def tearDown(self):
         pass
 
     def test_constant(self):
         constant_func = distribution_functions['constant']
-        ans = constant_func(mean = 54,n=2)
-        #print "ans", ans
-        actual = [54.,54.]
-        self.failUnless(ans==actual,
+        ans = constant_func(mean=54, n=2)
+        # print "ans", ans
+        actual = [54., 54.]
+        self.failUnless(ans == actual,
                         'Failed!')
 
     def test_uniform(self):
         func = distribution_functions['uniform']
-        ans = func(20,30, n=3)
-        #print "ans test_uniform", ans
+        ans = func(20, 30, n=3)
+        # print "ans test_uniform", ans
         actual = [27.28207076565548, 23.884363318551443, 27.350381515484784]
         actual = [24.523795535098188, 25.597723860804962, 29.242105840237294]
-        self.failUnless(ans==actual,
+        self.failUnless(ans == actual,
                         'Failed!')
 
-       
-    def test_normal(self): 
+    def test_normal(self):
         func = distribution_functions['normal']
-        ans = func(10,1, n=3)
-        #print "ans test_normal", ans
+        ans = func(10, 1, n=3)
+        # print "ans test_normal", ans
         actual = [8.7759273242860356, 10.377588198301599, 10.994999627670939]
-        self.failUnless(ans==actual,
+        self.failUnless(ans == actual,
                         'Failed!')
-       
-    def test_normalB(self): 
+
+    def test_normalB(self):
         func = distribution_functions['normal']
-        ans = func(10,10, n=3)
-        #print "ans test_normalB", ans
+        ans = func(10, 10, n=3)
+        # print "ans test_normalB", ans
         actual = [4.8679005141056466, -3.2897920068630899, 9.344705949865638]
         actual = [-2.240726757139651, 13.775881983015978, 19.949996276709399]
-        self.failUnless(ans==actual,
+        self.failUnless(ans == actual,
                         'Failed!')
 
-    def test_normalC(self): 
+    def test_normalC(self):
         func = distribution_functions['normal']
-        ans = func(10,10, n=3 ,minimum=9,maximum=9.2)
-        #print "ans test_normalC", ans
+        ans = func(10, 10, n=3, minimum=9, maximum=9.2)
+        # print "ans test_normalC", ans
         actual = [9.0733009505271482, 9.1829822262389467, 9.1365862664274857]
-        self.failUnless(ans==actual,
+        self.failUnless(ans == actual,
                         'Failed!')
-        
-    def test_lognormal(self): 
+
+    def test_lognormal(self):
         func = distribution_functions['normal']
-        ans = func(10,1, n=3)
-        #print "ans test_lognormal", ans
+        ans = func(10, 1, n=3)
+        # print "ans test_lognormal", ans
         actual = [8.7759273242860356, 10.377588198301599, 10.994999627670939]
-        self.failUnless(ans==actual,
+        self.failUnless(ans == actual,
                         'Failed!')
-       
-    def test_lognormalB(self): 
+
+    def test_lognormalB(self):
         func = distribution_functions['normal']
-        ans = func(10,10, n=3)
-        #print "ans test_lognormalB", ans
+        ans = func(10, 10, n=3)
+        # print "ans test_lognormalB", ans
         actual = [-2.240726757139651, 13.775881983015978, 19.949996276709399]
-        self.failUnless(ans==actual,
+        self.failUnless(ans == actual,
                         'Failed!')
 
-    def test_lognormalC(self): 
+    def test_lognormalC(self):
         func = distribution_functions['normal']
-        ans = func(10,10, n=3 ,minimum=9,maximum=9.2)
-        #print "ans test_normalC", ans
+        ans = func(10, 10, n=3, minimum=9, maximum=9.2)
+        # print "ans test_normalC", ans
         actual = [9.0733009505271482, 9.1829822262389467, 9.1365862664274857]
-        self.failUnless(ans==actual,
+        self.failUnless(ans == actual,
                         'Failed!')
 
-    def test_catagory(self): 
+    def test_catagory(self):
         func = distribution_functions['catagory']
-        answers = func(n=10,a=0.7,b=0.2,c=0.1)
-        #print "ans test_catagory", ans
+        answers = func(n=10, a=0.7, b=0.2, c=0.1)
+        # print "ans test_catagory", ans
         actuals = ['c', 'a', 'a', 'c', 'a', 'a', 'a', 'c', 'a', 'a']
         for answer, actual in map(None, answers, actuals):
-            self.failUnless(answer==actual, 'Failed!')
-        
+            self.failUnless(answer == actual, 'Failed!')
+
 #-------------------------------------------------------------
 if __name__ == "__main__":
-    suite = unittest.makeSuite(Test_Distributions,'test')
+    suite = unittest.makeSuite(Test_Distributions, 'test')
     runner = unittest.TextTestRunner()
     runner.run(suite)
