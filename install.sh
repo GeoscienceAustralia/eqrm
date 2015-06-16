14ad86864fb9eff062cbe37309362d668345544f
dunkgray <duncan.gray@ga.gov.au>
2015-06-16 14:35:29 +1000

    working on install procedure

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
+#!/bin/bash
 sudo apt-get update -qq
-sudo apt-get install build-essential
-sudo apt-get install libhdf5-serial-dev
-sudo apt-get install libnetcdf-dev
-sudo apt-get install python-dev
-sudo apt-get install libgeos-c1 libgeos-dev
-sudo apt-get install libblas-dev liblapack-dev
-sudo apt-get install gfortran
-
-sudo pip install numpy
-sudo pip install scipy
-sudo pip install matplotlib
-sudo pip install shapely
+sudo apt-get install build-essential -y
+sudo apt-get install libhdf5-serial-dev -y
+sudo apt-get install libnetcdf-dev -y
+sudo apt-get install python-dev -y
+sudo apt-get install libgeos-c1 libgeos-dev -y
+sudo apt-get install libblas-dev liblapack-dev -y
+sudo apt-get install gfortran -y
+sudo apt-get install python-pip
+sudo pip install numpy -y
+sudo pip install scipy -y
+sudo pip install matplotlib -y
+sudo pip install shapely -y
 #sudo pip install conda
 #sudo conda init
 #deps='pip numpy scipy matplotlib basemap shapely nose coverage'
