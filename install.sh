952f3f72ecc62039318cea6b9303e912b0adafb9
dunkgray <duncan.gray@ga.gov.au>
2015-06-17 12:00:04 +1000

    dos2unixing files

--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@ sudo apt-get install libgeos-c1 libgeos-dev -y
 sudo apt-get install libblas-dev liblapack-dev -y
 sudo apt-get install gfortran -y
 sudo apt-get install python-pip -y
+sudo apt-get install python-matplotlib -y
+sudo apt-get install libcr-dev mpich2 mpich2-doc -y
 sudo pip install numpy
 sudo pip install scipy
-sudo pip install matplotlib
 sudo pip install shapely
 #sudo pip install conda
 #sudo conda init
